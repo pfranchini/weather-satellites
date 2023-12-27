416e54b3388533e5e7d4ed6c36c8f155bca73d65
Paolo Franchini <pfranchini@gmail.com>
2023-12-27 18:50:44 +0100

    old improvements of apt.sh

--- apt.sh
+++ apt.sh
@@ -32,11 +32,45 @@ enhancement() {
 
 }
 
-enhancement_IR() {
+enhancement_HVC() {
 
     option=$1
     name=$2
     
+    echo -e ${option}":"
+
+    if [[ "$direction" == "northbound" ]]; then
+	$wxdir/wxtoimg -N -k "%N" -k "%d/%m/%Y - %H:%M UTC" -k "fontsize=14 %D %E %z" -k "fontsize=14 ${option}" -e ${option} -c -o ${file}_res.wav ${file}-${name}.png &> ${name}.log
+	#    $wxdir/wxtoimg -k "%N" -k "%d/%m/%Y - %H:%M UTC" -k "fontsize=14 %D %E %z" -k "fontsize=14 ${option}" -e ${option} -c -y low -o ${file}_res.wav ${file}-${name}.png &> ${name}.log
+    else
+	$wxdir/wxtoimg -S -k "%N" -k "%d/%m/%Y - %H:%M UTC" -k "fontsize=14 %D %E %z" -k "fontsize=14 ${option}" -e ${option} -c -o ${file}_res.wav ${file}-${name}.png &> ${name}.log
+    fi
+    touch -r ${file}.wav ${file}-${name}.png
+    cat ${name}.log
+
+    # Remove a wrong enhancement for missed channel
+    error=`cat ${name}.log | grep -i "warning: enhancement ignored"`
+    if  [ ! -z "$error" ]; then
+	rm ${file}-${name}.png
+	echo ${file} $error >> errors.log
+    else
+	# Preserve in /deleted any other warning
+	error=`cat ${name}.log | grep -i "warning"`
+	if  [ ! -z "$error" ]; then
+            mv ${file}-${name}.png $output/noaa/deleted/
+            echo ${file} $error >> errors.log
+	fi
+    fi
+    
+    rm -rf ${name}.log
+
+}
+
+enhancement_IR() {
+
+    option=$1
+    name=$2
+
     echo -e ${option}":"
 $wxdir/wxtoimg -m ${file}-map.png -k "%N" -k "%d/%m/%Y - %H:%M UTC" -k "fontsize=14 %D %E %z" -k "fontsize=14 ${name}" -b -e ${option} -c -o ${file}_res.wav ${file}-${name}.png &> ${name}.log
 
@@ -96,12 +130,13 @@ touch -r ${file}.wav ${file}_res.wav
 # map to overlay
 $wxdir/wxmap -c g:dark-cyan -c C:light-green -a -T "$satellite" -H $tle -p 0 -l 1 -o $start ${file}-map.png &> wxtoimg.log
 cat wxtoimg.log
+direction=`less wxtoimg.log  | grep Direction | awk '{print $2}'`
 
 # IR:
 enhancement_IR histeq IR
 
 # VIS:
-enhancement HVC HVC
+enhancement_HVC HVC HVC
 enhancement MSA MSA
 enhancement MSA-precip MSA-precip
 enhancement HVCT HVCT
@@ -112,7 +147,7 @@ rm -rf ${file}_res.wav
 rm -rf wxtoimg.log
 
 
-#direction=`less wxtoimg.log  | grep Direction | awk '{print $2}'`
+
 #if [[ "$direction" == "southbound" ]]; then
 #   echo "Rotate: "
 #   convert -rotate 180 $file-IR.png $file-IR.png
